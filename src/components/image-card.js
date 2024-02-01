785c640ee45f7f9670f16eb2c952e1ae996d70eb
Rik Smith-Unna [@blahah] <richardsmith404@gmail.com>
2024-02-01 01:52:06 +0000

    fix menu, adjust fonts, opacity

--- src/components/image-card.js
+++ src/components/image-card.js
@@ -11,7 +11,7 @@ export const ImageCard = ({imgUrl, caption, linkUrl}) => {
   return (
     <a className="image-card w-full md:w-1/2 lg:w-1/3" href={linkUrl}>
       <div style={imgStyle}>
-        <div className="image-caption absolute left-0 bottom-0 bg-black px-1 text-white">
+        <div className="image-caption absolute left-0 bottom-0 bg-black/70 px-1 text-white">
           {caption}
         </div>
       </div>
--- src/components/navbar.js
+++ src/components/navbar.js
@@ -3,8 +3,8 @@ export const Navbar = () => {
     <div className="navbar bg-base-100">
       <div className="navbar-start">
         <div className="dropdown">
-          <div tabIndex={0} role="button" className="btn btn-ghost md:hidden lg:hidden">
-            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 6h8m-8 6h16" /></svg>
+          <div tabIndex={0} role="button" className="btn p-3 btn-ghost md:hidden lg:hidden">
+            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
           </div>
           <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 w-52">
             <li><a href="/about">ABOUT</a></li>
@@ -12,7 +12,7 @@ export const Navbar = () => {
           </ul>
         </div>
         <a
-          className="text-nowrap md:pl-4 lg:pl-4"
+          className="text-nowrap ml-3 sm:ml-0 md:pl-4 lg:pl-4"
           href="/"
         >
           RODRIGO MORÁN
